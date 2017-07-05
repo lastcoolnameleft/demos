f61010432252f2d374f586ed111ebf399602aaec
Tommy Falgout <tommy@lastcoolnameleft.com>
2017-07-05 12:03:40 -0500

    Cleanup k8s script some more

--- k8s-on-azure/demo.sh
+++ k8s-on-azure/demo.sh
@@ -2,19 +2,22 @@
 
 . ../setup.sh
 
-RESOURCE_GROUP=sandbox-k8s
+RESOURCE_GROUP=tmp-k8s
 
 desc "Deploy the Infrastructure for the Kubernetes cluster"
 
 desc "Run in a separate window:"
 desc "az group create -n $RESOURCE_GROUP -l southcentralus"
 desc "time az acs create -g $RESOURCE_GROUP -n $RESOURCE_GROUP -t Kubernetes --admin-username thfalgou"
-desc "open https://ms.portal.azure.com/#resource/subscriptions/df8428d4-bc25-4601-b458-1c8533ceec0b/resourceGroups/$RESOURCE_GROUP/overview"
+run ""
 
-#../python-docker/demo.sh
+../python-docker/demo.sh
 
 # From now on, assuming we have a working K8S Cluster on Azure
 
+desc "...Is the Kubernetes cluster up yet?"
+run ""
+run "open https://ms.portal.azure.com/#resource/subscriptions/df8428d4-bc25-4601-b458-1c8533ceec0b/resourceGroups/$RESOURCE_GROUP/overview"
 desc "Pull Kubernetes config down to local host"
 run "az acs kubernetes get-credentials -g $RESOURCE_GROUP -n $RESOURCE_GROUP"
 desc "From now on, we can use Kubernetes native cli"
@@ -58,9 +61,11 @@ run "curl $SERVICE_IP"
 
 desc "What if you want to install not just a single docker image, but an entire application?"
 desc "For example, MediaWiki, which has both the application + the DB."
+run "helm init"
+run "open https://helm.sh/"
 run "helm install stable/mediawiki"
 
-desc "open http://github.com/kubernetes/charts/tree/master/stable"
+run "open http://github.com/kubernetes/charts/tree/master/stable"
 
 desc "What if we want to run something more interesting..."
 run "helm install --set minecraftServer.eula=true stable/minecraft"
@@ -70,3 +75,4 @@ desc "Let's talk about monitoring containers"
 run "open https://thfalgou-k8s.portal.mms.microsoft.com"
 run "open https://docs.microsoft.com/en-us/azure/container-service/container-service-kubernetes-oms"
 
+desc "Hope you enjoyed!"
--- python-docker/app.py
+++ python-docker/app.py
@@ -17,7 +17,7 @@ def hello():
 
     html = "<h3>Hello {name}!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>" \
-           "<b>Visits:</b> {visits}"
+           "<b>Visits:</b> {visits}\n"
     return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
 
 if __name__ == "__main__":
