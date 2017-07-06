ab4af9d2761311da48dd427a0f2e54e82ca951a9
Tommy Falgout <tommy@lastcoolnameleft.com>
2017-07-06 15:28:43 -0700

    demo of draft

--- draft/cleanup.sh
+++ draft/cleanup.sh
@@ -1,4 +1,9 @@
-git reset --hard
-git clean -df
+#git reset --hard
 helm delete --purge draft
 kubectl delete -n kube-system svc/draftd
+export DRAFT_APP_NAME=$(cat draft.toml | toml | jq ".environments.development.name" --raw-output)
+kubectl delete deploy/$DRAFT_APP_NAME-$DRAFT_APP_NAME
+kubectl delete service/$DRAFT_APP_NAME-$DRAFT_APP_NAME
+
+# Must come after 
+git clean -df
--- draft/demo.sh
+++ draft/demo.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+# Requires https://www.npmjs.com/package/toml-cli
+
 . ../setup.sh
 
-RESOURCE_GROUP=sandbox-k8s
-ACR_NAME=thfalgoutdemoacr
+RESOURCE_GROUP=${RESOURCE_GROUP:-sandbox-k8s}
+ACR_NAME=${ACR_NAME:-thfalgoudemoacr}
+BASE_DOMAIN=${BASE_DOMAIN:-draft.local}
 
 desc "Demo of Kubernetes Draft"
 if [ -z "$TMUX" ]; then echo "Make sure you are in tmux" && exit 1; fi
+if [ -z "$SUDO_COMMAND" ]; then echo "Need to run as sudo for modifying /etc/hosts" && exit 1; fi
 
 desc "If you're awesome, you're already using K8S on ACS and you can pull down the K8S Config easily:"
 desc "az acs kubernetes get-credentials -g $RESOURCE_GROUP -n $RESOURCE_GROUP"
@@ -17,7 +21,7 @@ desc "Draft uses Helm to deploy"
 run "helm init"
 run "helm install stable/nginx-ingress --namespace=kube-system --name=nginx-ingress"
 
-#run "draft init"
+run "draft init"
 
 run "cd python"
 run "ls"
@@ -30,26 +34,24 @@ run "cat draft.toml"
 run "cat Dockerfile"
 
 tmux split-window -v
-tmux select-layout even-vertical
-tmux select-pane -t 0
-
+tmux split-window -h
 tmux send-keys -t 1 "draft up"
+tmux send-keys -t 2 "watch kubectl get all"
 
+tmux select-pane -t 0
 read -s
 
 desc "Don't forget to update /etc/hosts with endpoint"
 desc "kubectl --namespace kube-system get services nginx-ingress-nginx-ingress-controller"
 run 'export SERVICE_IP=$(kubectl --namespace kube-system get services nginx-ingress-nginx-ingress-controller --output json | jq --raw-output ".status.loadBalancer.ingress[0].ip")'
+run 'export DRAFT_APP_NAME=$(cat draft.toml | toml | jq ".environments.development.name" --raw-output)'
 run 'echo $SERVICE_IP'
-desc "Add to /etc/hosts: $SERVICE_IP <draft host>"
-
-run ""
-run "sudo vi /etc/hosts"
+run 'echo $DRAFT_APP_NAME'
+run "echo $SERVICE_IP $DRAFT_APP_NAME.$BASE_DOMAIN | sudo tee /etc/hosts"
 
 desc "Now make a change to the application"
 run "vi app.py"
 
 desc "Draft will package and deploy the new application"
-desc "In other window: watch curl -s <draft host>"
-run "watch kubectl get all"
+run "watch curl -s $DRAFT_APP_NAME.$BASE_DOMAIN"
 
