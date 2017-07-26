3d2420b87015c5ec07767a5079cae083e1515023
Tommy Falgout <tommy@lastcoolnameleft.com>
2017-07-26 15:31:58 -0500

    fix container_name

--- azure-container-instance/demo.sh
+++ azure-container-instance/demo.sh
@@ -4,22 +4,23 @@
 
 RESOURCE_GROUP=${RESOURCE_GROUP:-sandbox-aci}
 CONTAINER_IMAGE=${CONTAINER_IMAGE:-microsoft/aci-helloworld}
+CONTAINER_NAME=${CONTAINER_NAME:-mycontainer}
 
 desc "This demo showcases the Azure Container Instance"
 desc "--> Create the Resource Group"
 run "az group create --name $RESOURCE_GROUP --location eastus"
 
 desc "--> Create the Container Instance"
-run "az container create --name $CONTAINER_IMAGE --image $CONTAINER_IMAGE --resource-group $RESOURCE_GROUP --ip-address public"
+run "az container create --name $CONTAINER_NAME  --image $CONTAINER_IMAGE --resource-group $RESOURCE_GROUP --ip-address public"
 
 desc "--> Wait for the instance to be available"
-run "az container show --name $CONTAINER_IMAGE --resource-group $RESOURCE_GROUP"
+run "az container show --name $CONTAINER_NAME --resource-group $RESOURCE_GROUP"
 
 desc "--> Fetch the Logs"
-run "az container logs --name $CONTAINER_IMAGE --resource-group $RESOURCE_GROUP"
+run "az container logs --name $CONTAINER_NAME --resource-group $RESOURCE_GROUP"
 
 desc "--> Fetch the IP"
-run 'export IP=$(az container show --name $CONTAINER_IMAGE --resource-group $RESOURCE_GROUP | | jq -r ".ipAddress.ip)"'
+run 'export IP=$(az container show --name $CONTAINER_NAME --resource-group $RESOURCE_GROUP | | jq -r ".ipAddress.ip)"'
 run 'echo $IP'
 
 desc "--> Curl the URL"
@@ -27,8 +28,8 @@ run "curl $IP"
 run "curl $IP"
 
 desc "--> Fetch the Logs"
-run "az container logs --name $CONTAINER_IMAGE --resource-group $RESOURCE_GROUP"
+run "az container logs --name $CONTAINER_NAME --resource-group $RESOURCE_GROUP"
 
 desc "--> Delete the container"
-run "az container delete --name $CONTAINER_IMAGE --resource-group $RESOURCE_GROUP"
+run "az container delete --name $CONTAINER_NAME --resource-group $RESOURCE_GROUP"
 
